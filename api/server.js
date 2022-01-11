7c1f114cdee0ce843efa1fcd1299ec55f4e5d11c
Aaron Reyes <56135909+aaronpear@users.noreply.github.com>
2022-01-10 19:00:32 -0800

    mostly complete thru third function

--- api/server.js
+++ api/server.js
@@ -1,3 +1,74 @@
 // BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const express = require('express');
+const User = require('./users/model.js');
+
+const server = express();
+
+server.use(express.json());
+
+
+server.post('/api/users', (req, res) => {
+    console.log(req.body);
+    const { name, bio } = req.body;
+    console.log(name, bio);
+    User.insert({ name, bio })
+        .then(resp => {
+            console.log(resp);
+            if (!name || !bio) {
+                res.status(400).json({ message: "Please provide name and bio for the user" });
+            } else {
+                res.status(201).json(resp);
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ message: "There was an error while saving the user to the database" });
+        });
+})    
+//     try {
+//       const { name, bio } = req.body;
+//       console.log(req.body);
+//       console.log(name, bio);
+//       if (!name || !bio) {
+//           res.status(400).json({ message: "Please provide name and bio for the user" });
+//       }
+//       const newUser = await User.insert({ name, bio });
+//       console.log(newUser);
+//       res.status(201).json(newUser);
+//     } catch (err) {
+//       res.status(500).json({ message: "There was an error while saving the user to the database" })
+//     }
+//   })
+
+server.get('/api/users', (req, res) => {
+    User.find()
+        .then(resp => {
+            console.log(resp);
+            res.json(resp);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    User.findById(req.params.id)
+        .then(resp => {
+            console.log(resp);
+            if (!resp) {
+                res.status(404).json({ message: "The user with the specified ID does not exist" });
+            } else {
+                res.json(resp);
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ message: "The user information could not be retrieved" });
+        })
+})
+  
+  
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
